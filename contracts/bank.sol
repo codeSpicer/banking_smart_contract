8a3256ffe5b5b8d347881b15c0d1132cd36a9f34
PAVILION\aksha <akshat.lko.2001@gmail.com>
2022-01-29 21:56:34 +0530

    updated bank.sol

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -10,10 +10,23 @@ contract bank {
         bool exists;
     }
 
+    uint256 bank_liquidity;
+    uint256 fees;
+
     mapping(address => user) public Users;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function collect_fees(address payable _receiver) public {
+        require(msg.sender == owner);
+        _receiver.transfer(fees);
+        fees = 0;
+    }
+
+    function show_fees() public view returns (uint256) {
+        return fees;
     }
 
     function register(string memory _name) public {
@@ -27,19 +40,27 @@ contract bank {
 
     function withdraw(uint256 _amount, address payable _receiver) external {
         require(_amount > 0 && _amount <= Users[msg.sender].balance);
-        _receiver.transfer(_amount);
-        Users[msg.sender].balance -= _amount;
+        uint256 amt = (_amount * 99) / 100;
+        _receiver.transfer(amt);
+        Users[msg.sender].balance -= amt;
+        fees += _amount - amt;
     }
 
     function transferTo(address payable _to, uint256 _amount) external {
         require(_amount > 0 && _amount <= Users[msg.sender].balance);
         require(Users[msg.sender].exists && Users[_to].exists);
+        uint256 amt = (_amount * 95) / 100;
         _to.transfer(_amount);
-        Users[msg.sender].balance -= _amount;
-        Users[_to].balance += _amount;
+        Users[msg.sender].balance -= amt;
+        Users[_to].balance += amt;
+        fees += _amount - amt;
     }
 
     function balanceOf() public view returns (uint256) {
         return Users[msg.sender].balance;
+    }
+
+    fallback() external {
+        revert();
     }
 }
